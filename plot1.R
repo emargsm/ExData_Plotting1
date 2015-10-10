9f2aaaee585dd934341af8cc9617eae1291f054d
Marguerite <marguerite.smith+github@gmail.com>
2015-10-10 22:53:36 +0100

    October submission of plot1 files. Fixed x-axis display this time.

--- plot1.R
+++ plot1.R
@@ -1,4 +1,4 @@
-## Exploratory Data Analysis, August 2015
+## Exploratory Data Analysis, October 2015
 ## Project 1, Graph 1
 ##
 ## Common info:
@@ -8,7 +8,7 @@
 ## Construct the plot and save it to a PNG file with a width of 480 pixels and a 
 ## height of 480 pixels
 ## Name each of the plot files as plot1.png, plot2.png, etc.
-## Converting dates?
+
 
 
 ## Plot 1
@@ -39,14 +39,11 @@ power_data$Date <- as.Date(strptime(power_data$Date, format = "%d/%m/%Y"))
 ## and will still group accordingly.
 
 png(filename = "plot1.png", width = 480, height = 480)
-hist(power_data$Global_active_power, breaks = seq(from = 0, to = ceiling(max(power_data$Global_active_power)), by=0.5),
-     main="Global active power",xlab = "Global active power (kilowatts)",col = "red")
+#hist(power_data$Global_active_power, breaks = seq(from = 0, to = ceiling(max(power_data$Global_active_power)), by=0.5),
+#     main="Global active power",xlab = "Global active power (kilowatts)",col = "red")
+hist(power_data$Global_active_power, main="Global active power", xlab = "Global active power (kilowatts)", col = "red")
 dev.off()
 
-## Note: My plot's x-axis units go up to 8, as there are actually a few instances
-## of kilowatts > 6. They're too few to show up on the y-axis, but it shunts 
-## the x-axis units out. I thought about using xlim(c(0,6)), but 
-## 1) that would artificially limit the data in a way that I believe is 
-##    unintended, and
-## 2) it still didn't look exactly like the sample, where the actual x-axis
-##    extends past the x-axis units
+## Note: I've left in the original hist() settings I used when submitting this in August, 
+## in part for my own notes and in part to show that I really did re-examine / re-do my 
+## submissions when re-taking the course in October.
